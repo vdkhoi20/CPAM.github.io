1d9f9606059959ab351b570b2baf81d9fd08587e
vdkhoi20 <vdkhoi.itai@gmail.com>
2025-12-24 12:32:37 +0700

    4th fixed

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  output: "export",
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'CPAM'; // Tên chính xác repo của bạn
+
+const nextConfig = {
+  output: 'export',
+  
+  // Quan trọng: Thêm tên repo vào đường dẫn khi chạy production (build)
+  // Nếu đang chạy local (npm run dev) thì không cần
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
